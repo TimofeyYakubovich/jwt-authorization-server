26966516276c4572f8eaa124a84345c9743469f2
Timofey <yakubovich96@mail.ru>
2023-06-13 00:18:13 +0300

    changed cors7

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cookieParser()); // что бы работал res.cookie
 app.use(cors({
     // надо указать с каким доеном ему надо обмениваться куками
     credentials: true, // разрешаем куки
-    origin: process.env.CLIENT_URL, // указываем юрл фронтенда
+    // origin: process.env.CLIENT_URL, // указываем юрл фронтенда
     // exposedHeaders: '*',
 
     origin: function(origin, callback){    // allow requests with no origin 
