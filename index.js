05763ed49544350bf167f263c93af3d9b54e427c
Timofey <yakubovich96@mail.ru>
2023-06-13 00:04:16 +0300

    changed cors3

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cors({
     // надо указать с каким доеном ему надо обмениваться куками
     credentials: true, // разрешаем куки
     // origin: process.env.CLIENT_URL, // указываем юрл фронтенда
-    // exposedHeaders: '*',
+    exposedHeaders: '*',
 
     origin: function(origin, callback){    // allow requests with no origin 
         // (like mobile apps or curl requests)
