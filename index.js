67eb11346839a714e8a5be051fa27de456876ed2
Timofey <yakubovich96@mail.ru>
2023-06-13 00:20:17 +0300

    changed cor87

--- index.js
+++ index.js
@@ -16,25 +16,25 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-var allowedOrigins = [rocess.env.CLIENT_URL];
+// var allowedOrigins = [rocess.env.CLIENT_URL];
 
 app.use(express.json());
 app.use(cookieParser()); // что бы работал res.cookie
 app.use(cors({
     // надо указать с каким доеном ему надо обмениваться куками
     credentials: true, // разрешаем куки
-    // origin: process.env.CLIENT_URL, // указываем юрл фронтенда
-    // exposedHeaders: '*',
+    origin: process.env.CLIENT_URL, // указываем юрл фронтенда
+    exposedHeaders: '*',
 
-    origin: function(origin, callback){    // allow requests with no origin 
-        // (like mobile apps or curl requests)
-        if(!origin) return callback(null, true);    
-        if(allowedOrigins.indexOf(origin) === -1){
-          var msg = 'The CORS policy for this site does not ' +
-                    'allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-        }    return callback(null, true);
-      }
+    // origin: function(origin, callback){    // allow requests with no origin 
+    //     // (like mobile apps or curl requests)
+    //     if(!origin) return callback(null, true);    
+    //     if(allowedOrigins.indexOf(origin) === -1){
+    //       var msg = 'The CORS policy for this site does not ' +
+    //                 'allow access from the specified Origin.';
+    //       return callback(new Error(msg), false);
+    //     }    return callback(null, true);
+    //   }
 }))
 app.use('/api', router)
 // когда подключаем мидлвеер для обработки ошибок он обезательно должен идти последним в цепочке мидлвееров
